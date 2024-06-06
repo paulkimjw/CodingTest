b3acf61afef0aebb8f3f3a63648e163b4e85adea
paulkimjw <119037642+paulkimjw@users.noreply.github.com>
2024-06-06 14:12:15 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,13 @@
+SELECT 
+    ai.ANIMAL_ID, 
+    ai.NAME
+FROM 
+    ANIMAL_INS ai
+INNER JOIN 
+    ANIMAL_OUTS ao
+ON 
+    ai.ANIMAL_ID = ao.ANIMAL_ID
+WHERE 
+    ao.DATETIME < ai.DATETIME
+ORDER BY 
+    ai.DATETIME;
