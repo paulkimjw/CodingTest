a9864edda676a38271f138b8dee1bbd9af760b8a
paulkimjw <119037642+paulkimjw@users.noreply.github.com>
2025-04-07 15:21:38 +0900

    [level 1] Title: 약수의 합, Time: 0.12 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,9 +1,14 @@
-def solution(n):
-    answer = n
-    for i in range(1,n//2+1) :
-        if n%i == 0:
-            answer += i
+# def solution(n):
+#     answer = n
+#     for i in range(1,n//2+1) :
+#         if n%i == 0:
+#             answer += i
     
             
     
-    return answer
+#     return answer
+
+def solution(n):
+    return n + sum([i for i in range(1,(n//2)+1) if n%i == 0])
+
+
